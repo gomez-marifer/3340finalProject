955af348df9023b0fe46b5844f3b22f54d521f5d
rubidium963 <Mgomez5221@gmail.com>
2024-10-12 16:20:15 -0500

    first commit

---
+++ mydb.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+	host = 'localhost',
+	user = 'root',
+	passwd = 'Classof24@',
+	)
+
+#prepare cursor object (using the connector declare above)
+cursorObject = dataBase.cursor() 
+
+#create data base
+cursorObject.execute("CREATE DATABASE projectData")
+
+#Message in console to see if it worked 
+print("Hello data base projectData")
