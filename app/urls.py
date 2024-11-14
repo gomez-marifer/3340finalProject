16d9f04d013f136359e9dbe01b85f3b95b86600e
rubidium963 <Mgomez5221@gmail.com>
2024-11-14 14:00:40 -0600

    Login and Logout

--- app/urls.py
+++ app/urls.py
@@ -2,17 +2,13 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('', views.home, name='home'),
-    path('login/',views.loginPage, name = 'login'),
-    path('register/',views.registerPage, name = 'register')
-=======
     #path('admin/', admin.site.urls),
     path('',views.home, name = 'home'),
-    path('login/',views.login, name = 'login'),
-    path('register/',views.register, name = 'register'),
+    path('login/',views.loginPage, name = 'login'),
+    path('register/',views.registerPage, name = 'register'),
     path('aboutUs/',views.aboutUs, name = 'aboutUs'),
     path('tasks/',views.tasks, name = 'tasks'),
-    path('administrator/',views.administrator, name = 'administrator')
->>>>>>> 2e57ce8a21565f8ea13c91f3e1b158aa14c632e7
+    path('administrator/',views.administrator, name = 'administrator'),
+    path('logout/', views.logoutUser, name='logout'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -7,60 +7,65 @@ from django.contrib.auth import authenticate,login, logout
 
 from django.contrib import messages
 
+from django.contrib.auth.decorators import login_required
 from .models import *
 from .forms import CreateUserForm
 
 # Create your views here.
 def registerPage(request):
-    form = CreateUserForm()
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
 
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, "Account was created for " + user)
-            
-            return redirect('login')
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, "Account was created for " + user)
+                
+                return redirect('login')
 
-    context = {'form':form}
-    return render(request, 'register.html', context)
+        context = {'form':form}
+        return render(request, 'register.html', context)
 
 
 def loginPage(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-        user = authenticate(request, username=username, password=password)
+            user = authenticate(request, username=username, password=password)
 
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.info(request, 'Username OR Password is Incorrect')        
-    context = {}
-    return render(request, 'login.html', context)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username OR Password is Incorrect') 
+                       
+        context = {}
+        return render(request, 'login.html', context)
 
+@login_required(login_url='login')
 def home(request):
-<<<<<<< HEAD
     context = {}
     return render(request, 'home.html', context)
-=======
-    return render(request, 'home.html',{})
-
-def login(request):
-    return render(request, 'login.html',{})
-
-def register(request):
-    return render(request, 'register.html',{})
 
 def aboutUs(request):
     return render(request, 'aboutUs.html',{})
 
+@login_required(login_url='login')
 def tasks(request):
     return render(request, 'tasks.html',{})
 
 def administrator(request):
     return render(request, 'admin.html',{})
->>>>>>> 2e57ce8a21565f8ea13c91f3e1b158aa14c632e7
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
